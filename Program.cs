b83c211028faa838500083cd557032091cd177ff
unknown <st10218995@vcconnect.edu.za>
2023-04-26 19:02:06 +0000

    prompt and for loop to enter the  details of recipe such as ingredient and step details

--- Program.cs
+++ Program.cs
@@ -10,6 +10,38 @@ namespace PROG6221_Part1
     {
         static void Main(string[] args)
         {
+
+            //prompt user to enter the ingredients
+            Console.WriteLine("Enter the number of ingredients:");
+            int numIngredients = int.Parse(Console.ReadLine());
+
+
+            //for loop to get the details of ingredients from the user 
+            for (int i = 0; i < numIngredients; i++)
+            {
+                Console.WriteLine($"\nEnter the name of ingredient {i + 1}: ");
+                string name = Console.ReadLine();
+
+                Console.WriteLine($"Enter the quantity of {name}: ");
+                double quantity = double.Parse(Console.ReadLine());
+
+                Console.WriteLine($"Enter the unit of measurement for {name}: ");
+                string unit = Console.ReadLine();
+
+            }
+
+            //prompt user to enter the number of steps 
+            Console.WriteLine("Enter the number of steps:");
+            int numSteps = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < numSteps; i++)
+            {
+                Console.WriteLine($"\nEnter step and description{i + 1}: ");
+                string step = Console.ReadLine();
+
+
+            }
+
         }
     }
 }
